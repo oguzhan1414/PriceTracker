89d3388248f3e56252a5f126d2dc7da223243e88
Oğuzhan Şekerci <oguzhansekerci14@gmail.com>
2026-03-30 20:30:17 +0300

    Update main.py

--- price_tracker/backend/main.py
+++ price_tracker/backend/main.py
@@ -172,8 +172,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000",
-                   "https://price-tracker-prt.vercel.app"], # Frontend adreslerin
+    allow_origins=["*"], # Frontend adreslerin
     allow_credentials=True, # BU ÇOK KRİTİK! (Biletlere izin ver)
     allow_methods=["*"],
     allow_headers=["*"],
