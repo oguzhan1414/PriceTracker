0340899b9a17b9e470e3d68b72537298cbad7047
Oğuzhan Şekerci <oguzhansekerci14@gmail.com>
2026-03-30 19:43:00 +0300

    Update vite.config.ts

--- price_tracker/frontend/vite.config.ts
+++ price_tracker/frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Frontend'den "/api" ile başlayan bir istek çıkarsa...
+      '/api': {
+        // ...bunu çaktırmadan Railway'deki canlı sunucuya yönlendir!
+        target: 'https://pricetracker-production-b887.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
